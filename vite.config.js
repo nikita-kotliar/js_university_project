86736f68c3053f1dbdc20933bb7d304663a9837e
nikita-kotliar <knr200710@gmail.com>
2026-01-17 19:02:03 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/js_university_project/', 
 
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
